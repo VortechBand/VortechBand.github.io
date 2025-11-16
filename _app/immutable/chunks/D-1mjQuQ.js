88744f7846eaf08a0231f7ecfc2c6f76c604f87b
Torniojaws <torniojaws@gmail.com>
2025-11-16 04:46:41 +0200

    Move to app.html

---
+++ _app/immutable/chunks/D-1mjQuQ.js
@@ -0,0 +1,1 @@
+import{M as a}from"./PkIJ9Hpu.js";a();
---
+++ _app/immutable/chunks/s7AvZCQt.js
@@ -0,0 +1,1 @@
+import{G as o,D as t,a7 as c,w as u}from"./PkIJ9Hpu.js";function a(n){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function r(n){t===null&&a(),c&&t.l!==null?l(t).m.push(n):o(()=>{const e=u(n);if(typeof e=="function")return e})}function l(n){var e=n.l;return e.u??(e.u={a:[],b:[],m:[]})}export{r as o};
