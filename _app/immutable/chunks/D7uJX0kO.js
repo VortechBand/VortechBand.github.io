0330355d3f7f3e2fa3245015370e83ab3d368c2d
Torniojaws <torniojaws@gmail.com>
2025-12-07 01:08:28 +0200

    Update news

---
+++ _app/immutable/chunks/D7uJX0kO.js
@@ -0,0 +1,51 @@
+const e=[{author:"",categories:["Release"],contents:`The new album has now been released! Go and have a listen to Void Emergent, our 9th full length album, over at <a href="https://vortech.bandcamp.com/album/void-emergent">Bandcamp</a> and soon in Spotify, Apple Music, Youtube, etc
+
+<img style="width: 100%;" src="/images/releases/VOR012-cover.jpg" alt="Void Emergent" />
+
+Tracklist:
+
+01. The Final 200
+02. Sinews
+03. Alien DNA
+04. Inside the Void
+05. Spiral Galaxy
+06. Augmentation
+07. The Nuclear Option
+08. Cybernetic Warrior
+09. Embers
+10. Transcend the Confines of Time
+11. Call of the Void
+
+A harrowing story of mankind's journey into the metaphorical void, for reasons that are purely human. Featuring main vocals once again by ICanBeFree, with further guest vocals by Andrés Arcentales, Carolina Padrón and Toni Hatakka!`,created:"2025-12-07 00:39:00",id:369,title:"Into the void",updated:null},{author:"",categories:["Release"],contents:"Just some final effects and adjustments left to do and the new album is a wrap! 11 songs of kicking futuristic metal with twists and turns, taking you to the infinite space and void!",created:"2025-11-04 11:24:00",id:368,title:"Final steps!",updated:null},{author:"",categories:["Release"],contents:"Final bits of vocals in progress, and then it is almost time! The songs are sounding very cool and fast, with some twists here and there! This one is more akin to Posthumanism and Of What Remains, and should please your need for speed... This time there are not one or two, but 4 vocalists taking part!",created:"2025-10-04 15:08:00",id:367,title:"Vocals nearly done",updated:null},{author:"",categories:["Release"],contents:'The new album now has 11 main vocals ready and 1/5 extra vocals done, and we are getting really close to release! While waiting, check out this teaser for one of the songs: <iframe style="width: 100%;aspect-ratio: 16/9;" src="https://www.youtube.com/embed/vCYzHvMhGnA" title="Alien DNA (Teaser 2025)" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',created:"2025-08-23 11:15:00",id:366,title:"The destination is in sight",updated:null},{author:"",categories:["Studio","recording"],contents:`It is finally the moment when recording the album vocals begins! We are once again joined by ICanBeFree who will provide the majority of the vocals for the album. And as mentioned earlier, there will be also some other surprises in the vocal department!
+
+The test mixes are sounding very nice, so once all vocals are ready, we should have a new album out in very short time. Possibly sometime around September 2025!`,created:"2025-07-20 13:55:00",id:365,title:"Vocal recording begins",updated:null},{author:"",categories:["Website","recording"],contents:`Work continues on the upcoming 9th album with demo vocals being finalized now, which will be the basis for the final vocals. All instruments are already ready to go, so we are getting very close to release!
+
+As mentioned previously, we will also have a new website starting from July, as the current service will end. It will still be in the same address <a href="https://vortechmusic.com">https://vortechmusic.com</a> however. But if are interested, you can already see an in-progress version of the website here: <a href="https://vortechband.github.io/">https://vortechband.github.io/</a>. The news there are just examples.
+
+I have added some new studio reports there, so now every release has a studio report. Some based on the older ones from around 2008 with some small additions here and there, and some reports are brand new. Have a look-see in the Music -> Album -> Studio report section in that new site.`,created:"2025-04-09 17:07:32",id:364,title:"Future website",updated:"2025-04-09 17:08:48"},{author:"Vortech",categories:["recording"],contents:`All bass tracks were finished earlier this month and they sound cool! Next up is finishing the lyrics and fine-tuning the demo vocals before the final vocals wiill be recorded. Getting close to release!
+
+There will also be some changes to the website later this year as it will move to another host. Probably around July.`,created:"2025-01-28 12:59:15",id:363,title:"Bass done, lyrics and vocals in progress",updated:"2025-01-28 13:01:05"},{author:"",categories:["recording"],contents:`The process to finalize all the new songs is now done, and on December 12 it is time to start recording the instruments for the 9th full length album, which is still untitled, but some possible names are "Dimensions Uncharted" or "Void Emergent".
+
+The tracklist right now is as follows, but the song titles might still change:
+
+01. The Final 200
+02. Sinews
+03. Alien DNA
+04. Inside the Void
+05. Spiral Galaxy
+06. Augmentation
+07. The Nuclear Option
+08. Cybernetic Warrior
+09. Embers
+10. Transcend the Confines of Time
+11. Call of the Void
+
+Clocking in at about 49 minutes. Most of the songs are further enhanced compared to the demo versions, with lots of new leads, updated drums and altered sections. Expect a bit more furious experience, with plenty of scifi.`,created:"2023-11-30 22:19:20",id:362,title:"Recording time!",updated:null},{author:"Juha",categories:["preproduction"],contents:`Not a surprise to some, but yes, it is indeed time to start the process that will end with the release of our next album! There is no working title for the 9th full length album yet, but the 11 songs are very much fully demoed and clocking in at around 50 minutes. Now it is time to start fleshing them out, adding all kinds of tricks, and writing the lyrics. Also known as preproduction. All things going smoothly, it should be out sometime in 2023...
+
+PS the work towards a website refresh is going on little by little, as time allows, and should please mobile users quite a bit!`,created:"2022-07-09 14:21:40",id:361,title:"Next album",updated:null},{author:"",categories:["Release"],contents:`At last, the 8th full length album, entitled The Shadow Presence is here. Also streaming in all major streaming services!
+
+<a href="https://vortech.bandcamp.com/album/the-shadow-presence">https://vortech.bandcamp.com/album/the-shadow-presence</a>
+
+13 tracks of dystopian future in a tight metal encasing of fast, futuristic metal! With 2 bonus tracks for the Tech Noir of dark city nights!
+
+Hang tight for a website update coming up soon!`,created:"2021-12-21 21:19:17",id:360,title:"The Shadow Presence is here",updated:"2022-01-01 11:03:41"},{author:"Juha",categories:["Release"],contents:"To hear the sounds in every possible environment and piece of equipment. And then it's time.",created:"2021-12-08 12:35:22",id:359,title:"A final listen",updated:"2021-12-08 12:37:03"},{author:"Juha",categories:["Release"],contents:"Things are moving, with vocals now in progress! With a preliminary mix already done, the vocals are pretty much the final step before release of The Shadow Presence. Should be quite close now...",created:"2021-04-22 10:43:06",id:358,title:"The Shadow Presence is imminent",updated:null},{author:"",categories:["Studio","recording","Release"],contents:"After quite a lot of work, the drums are now finalized! Now it is time to focus on final tweaks and some exciting vocals. Should be quite smooth sailing from here on, towards the release of the new full length album, with the album title expected to be: The Shadow Presence",created:"2021-01-24 13:29:34",id:357,title:"Drums done",updated:null},{author:"",categories:["Studio","recording"],contents:"Halfway through with the final drums for the next release!",created:"2020-05-18 14:32:47",id:356,title:"Drumtastic",updated:null}];export{e as n};
---
+++ _app/immutable/entry/start.D4MknVgU.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/Jb87xPOb.js";export{o as load_css,r as start};
